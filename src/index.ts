1af2ee471b5f5ae81b94c64a118bb66d682238f5
pikooli <>
2022-04-03 20:35:04 +0200

    look like it work

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-export type VariableType = {
-  any: any;
-};
-
-export default function test(variableTest: VariableType) {
-  // variableTest.any this work
-  console.log(variableTest, "Test in ts");
-}
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export type VariableType = {
+  any: any;
+};
+
+export default function Test() {
+  // variableTest.any this work
+  return <h1>yeah</h1>;
+}
