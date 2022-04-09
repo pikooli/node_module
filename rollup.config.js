fb4a22adb18aa2045b23d714df5852a91b18a492
pikooli <>
2022-04-09 21:07:41 +0200

    rollup build but still got useRef error

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
-import typescript from "rollup-plugin-typescript2";
-import commonjs from "rollup-plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+
 import external from "rollup-plugin-peer-deps-external";
 import resolve from "rollup-plugin-node-resolve";
 
 import pkg from "./package.json";
 
 export default {
-  input: "src/index.tsx",
+  input: "src/index.ts",
   output: [
     {
       file: pkg.main,
@@ -24,22 +25,7 @@ export default {
   plugins: [
     external(),
     resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      exclude: "**/__tests__/**",
-      clean: true,
-    }),
-    commonjs({
-      include: ["node_modules/**"],
-      namedExports: {
-        "node_modules/react/react.js": [
-          "Children",
-          "Component",
-          "PropTypes",
-          "createElement",
-        ],
-        "node_modules/react-dom/index.js": ["render"],
-      },
-    }),
+    typescript({ tsconfig: "./tsconfig.json" }),
+    commonjs(),
   ],
 };
